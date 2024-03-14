ab4c10f273538059c40aff9270fb0fae294b94a9
javascripty <ed2711uribe@gmail.com>
2024-03-14 12:40:51 -0700

    Update script command.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Contact from './components/Contact';
 import Dashboard from './components/Dashboard';
 import Product from './components/Product';
 
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 const router = createBrowserRouter([
